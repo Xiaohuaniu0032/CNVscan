82cc05caee5409803311921b213d842d201258b8
fulongfei <lffu_0032@outlook.com>
2021-03-03 17:18:43 +0800

    add QC check

--- bin/statQC.pl
+++ bin/statQC.pl
@@ -4,41 +4,52 @@ use File::Basename;
 
 
 # https://www.qiagen.com/us/applications/next-generation-sequencing/dna-sequencing
+# see above link to see what `Uniformity` is
 # 2021/3/3
+# 
 #
 #
-#
+
+my $cutoff = 0.8; # 0.5X cutoff
+
 my ($depth,$outdir) = @ARGV; # *.depth.tmp
 
 my $name = (split /\./, basename($depth))[0];
 my $of = "$outdir/$name\.CNV.QC.xls";
 
 open O, ">$of" or die;
-print O "sample\tmean_depth\t0.2X_mean_cov\t0.5X_mean_cov\n";
+print O "sample\tmean_depth\t0.2X_mean_cov\t0.5X_mean_cov\tUniformity_Check\n";
 
 # cal mean depth
-my @depth;
+my @base;
+my $cap_len;
+my $target_n = 0;
+
 open IN, "$depth" or die;
 <IN>;
 while (<IN>){
 	chomp;
 	my @arr = split /\t/;
-	push @depth, $arr[-2];
+	my $len = $arr[2] - $arr[1]; # target len
+	my $bn = $len * $arr[-2];
+	push @base, $bn;
+	$cap_len += $len;
+	$target_n += 1;
 }
 close IN;
 
-my $target_n = @depth;
-
-my $sum_depth = 0;
-for (@depth){
-	$sum_depth += $_;
+my $sum_base = 0;
+for (@base){
+	$sum_base += $_;
 }
 
+my $target_n = @depth;
+
 my $mean_depth;
-if ($sum_depth == 0){
+if ($sum_base == 0){
 	$mean_depth = 0;
 }else{
-	$mean_depth = sprintf "%.2f", $sum_depth/$target_n;
+	$mean_depth = sprintf "%.2f", $sum_base/$cap_len;
 }
 
 # cal 0.2X / 0.5X
@@ -64,6 +75,13 @@ close IN;
 my $x1_pct = sprintf "%.2f", $x1_cov_n / $target_n; # 99.5%
 my $x2_pct = sprintf "%.2f", $x2_cov_n / $target_n; # 96%
 
-print O "$name\t$mean_depth\t$x1_pct\t$x2_pct\n";
+my $qc;
+if ($x2_pct >= $cutoff){
+	$qc = "Pass";
+}else{
+	$qc = "Fail";
+}
+
+print O "$name\t$mean_depth\t$x1_pct\t$x2_pct\t$qc\n";
 close O;
 
