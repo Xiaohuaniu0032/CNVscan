69f785dfdc895208cf372c2d80bbc36725966afd
fulongfei <lffu_0032@outlook.com>
2021-03-05 11:00:20 +0800

    update test run sh

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,10 @@
-if [ ! -d $PWD/BB20062440_CL01167 ];then
-	mkdir $PWD/BB20062440_CL01167
+bam='/data3/Projects/panel889/210225_A00869_0418_BHWNFGDSXY/02_aln/210222048T.rmdup.bam'
+bed='/home/wangce/workdir/database/humandb/panel/889genes_20191225.bed'
+name='210222048T'
+ref='/home/fulongfei/workdir/git_repo/CNVscan/ref'
+
+if [ ! -d $PWD/$name ];then
+	mkdir $PWD/$name
 fi
 
-python ../CNVscan.py -bam /data2/Projects/genetic_tumor_338/200627_A00262_0472_AHCWFWDSXY/02_aln/BB20062440_CL01167.rmdup.bam -bed /data1/workdir/fulongfei/genetic/GeExCNV_db/338/bed/338genes_20191225.bed -n BB20062440_CL01167 -m cnv -od $PWD/BB20062440_CL01167
+python ../CNVscan.py -bam $bam -bed $bed -n $name -m cnv -od $PWD/$name -ref $ref
