9d2c7ca105df7e98526779823411d4b130fbefa9
fulongfei <lffu_0032@outlook.com>
2021-03-05 11:00:01 +0800

    remove log2ratio, use CN directly

--- bin/cal_logR.pl
+++ bin/cal_logR.pl
@@ -14,31 +14,41 @@ open IN, "$ref" or die;
 while (<IN>){
     chomp;
     my @arr = split /\t/;
-    my $t = shift @arr;
+    my $t = shift @arr; # 1.2488036-2488231.TNFRSF14 => chr.start-end.gene
     my $med = &median_arr(\@arr);
-    $ref{$t} = $med;
+    $ref{$t} = $med; # each target's median normalized value
 }
 close IN;
 
 #print(Dumper(\%ref));
 
 open O, ">$outfile" or die;
-print O "sample\tchr\tstart\tend\tgene\traw_depth\tgc\tgc_corrected_depth\tnorm_depth\tref_median_depth\tlog2Ratio\n";
+print O "sample\tchr\tstart\tend\tgene\traw_depth\tgc\tgc_corrected_depth\tnorm_depth\tref_median_depth\tcopynumber\n";
 open IN, "$depth" or die;
 <IN>;
 while (<IN>){
     chomp;
     my @arr = split /\t/;
     my $t = "$arr[0]\.$arr[1]\-$arr[2]\.$arr[3]"; # chr.start-end.gene
-    my $ref_median = $ref{$t};
+    my $ref_median = $ref{$t}; # median may be 
 
-    my $log2R;
+#    my $log2R;
+#    if ($arr[-1] == 0 || $ref_median == 0){
+#        $log2R = "NA"
+#    }else{
+#        $log2R = &log2($arr[-1]/$ref_median);
+#    }
+#
+#
+    # calculate copy number
+    my $cn;
     if ($arr[-1] == 0 || $ref_median == 0){
-        $log2R = "NA"
-    }else{
-        $log2R = &log2($arr[-1]/$ref_median);
+        $cn = "NA";
+	}else{
+        $cn = sprintf "%.2f", ($arr[-1] / $ref_median * 2);
     }
-    print O "$name\t$arr[0]\t$arr[1]\t$arr[2]\t$arr[3]\t$arr[4]\t$arr[-3]\t$arr[-2]\t$arr[-1]\t$ref_median\t$log2R\n";
+	
+    print O "$name\t$arr[0]\t$arr[1]\t$arr[2]\t$arr[3]\t$arr[4]\t$arr[-3]\t$arr[-2]\t$arr[-1]\t$ref_median\t$cn\n";
 }
 close IN;
 close O;
@@ -47,10 +57,10 @@ close O;
 
 
 
-sub log2{
-    my $n = shift;
-    return log($n)/log(2);
-}
+#sub log2{
+#    my $n = shift;
+#    return log($n)/log(2);
+#}
 
 
 sub median_arr{
@@ -60,9 +70,9 @@ sub median_arr{
     if (scalar(@val) % 2 == 0){
         my $v1 = $val[scalar(@val)/2-1];
         my $v2 = $val[scalar(@val)/2];
-        $median = sprintf "%.2f", ($v1+$v2)/2;
+        $median = sprintf "%.3f", ($v1+$v2)/2;
     }else{
-        $median = sprintf "%.2f", $val[ceil(scalar(@val)/2)];
+        $median = sprintf "%.3f", $val[ceil(scalar(@val)/2)];
     }
 
     return($median);
